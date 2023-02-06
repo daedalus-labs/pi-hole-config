fd5ccd3cb9c9600f1900cff8ea076a006b3809de
Joe Porembski <jporembski@gmail.com>
2023-02-05 23:10:46 -0500

    Stash #2

--- update.py
+++ update.py
@@ -1,13 +1,9 @@
-import json
 import logging
 import os
 import sqlite3
 import subprocess
-from typing import Final
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError, URLError
 
-STANDARD_COMMENT = "pi-hole-config: "
+STANDARD_COMMENT = "(pi-hole-config) {}"
 REGEX_INDEX = 0
 COMMENT_INDEX = 1
 GROUPS_INDEX = 2
@@ -26,10 +22,10 @@ def verify(path):
         bool: True if the path exists and is accessible, False otherwise.
     """
     if not os.path.exists(path):
-        logging.error(f"{path} was not found")
+        logging.error("{} was not found".format(path))
         return False
     if not os.access(path, os.X_OK | os.W_OK):
-        logging.error("Write access is not available for. Please run as root or other privileged user".format(path))
+        logging.error("Write access is not available for {}. Please run as root or other privileged user".format(path))
         return False
     return True
 
@@ -50,44 +46,50 @@ def read(path):
         regex_list.update(x for x in map(str.strip, regex_list.splitlines()) if x and x[:1] != "#")
     return regex_list
 
+
 def parse(config_list):
     """
     Converts a list of regular expressions read in from a csv into a list of tuples
     of the form: [<regular expression>, <comment>, <associated groups>]
 
     Args:
-        config_list (list): The list of regular expressions read in from the csv 
+        config_list (list): The list of regular expressions read in from the csv
 
     Returns:
         list: A list of tuples containing the parsed values from config_list
     """
     parsed_list = list()
     for config_item in config_list:
-        config_items = config_item.split(',')
-        if(len(config_items)) < 3:
+        config_items = config_item.split(",")
+        if (len(config_items)) < 3:
             continue
         parsed_list.append(tuple(config_items[REGEX_INDEX], config_items[COMMENT_INDEX], config_items[GROUPS_INDEX]))
     return parsed_list
 
+
+def add(cursor, parsed_object):
+    logging.info("Adding / updating {} to DB".format(parsed_object))
+    comment = STANDARD_COMMENT.format(parsed_object[COMMENT_INDEX])
+    cursor.execute(
+        "INSERT OR IGNORE INTO domainlist (type, domain, enabled, comment)",
+        "VALUES (3, ?, 1, ?)",
+        parsed_object[REGEX_INDEX],
+        comment,
+    )
+    cursor.execute(
+        "UPDATE domainlist " "SET comment = ? WHERE domain in (?) AND comment != ?",
+        comment,
+        parsed_object[REGEX_INDEX],
+        comment,
+    )
+
+
 def connect():
-    # Create a DB connection
-    logging.info("Connecting to {}".format(path_pihole_db))
-
-    try:
-        conn = sqlite3.connect(path_pihole_db)
-    except sqlite3.Error as e:
-        logging.error(e)
-        exit(1)
-
-    # Create a cursor object
-    c = conn.cursor()
-
     # Add / update remote regexps
     logging.info("Adding / updating regexps in the DB")
 
     c.executemany(
-        "INSERT OR IGNORE INTO domainlist (type, domain, enabled, comment) "
-        "VALUES (3, ?, 1, ?)",
+        "INSERT OR IGNORE INTO domainlist (type, domain, enabled, comment) " "VALUES (3, ?, 1, ?)",
         [(x, install_comment) for x in sorted(block_list)],
     )
     c.executemany(
@@ -95,12 +97,8 @@ def connect():
         [(install_comment, x, install_comment) for x in sorted(block_list)],
     )
 
-    conn.commit()
-
     # Fetch all current mmotti regexps in the local db
-    c.execute(
-        "SELECT domain FROM domainlist WHERE type = 3 AND comment = ?", (install_comment,)
-    )
+    c.execute("SELECT domain FROM domainlist WHERE type = 3")
     regexps_mmotti_local_results = c.fetchall()
     regexps_mmotti_local.update([x[0] for x in regexps_mmotti_local_results])
 
@@ -135,7 +133,8 @@ def connect():
 
     conn.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Set paths
     path_pihole = "/etc/pihole"
     path_pihole_db = os.path.join(path_pihole, "gravity.db")
@@ -150,3 +149,24 @@ if __name__ == '__main__':
 
     block_list = read("regex/block.csv")
     parsed_list = parse(block_list)
+
+    print(parsed_list)
+
+    # # Create a DB connection
+    # logging.info("Connecting to {}".format(path_pihole_db))
+
+    # try:
+    #     conn = sqlite3.connect(path_pihole_db)
+    # except sqlite3.Error as e:
+    #     logging.error(e)
+    #     exit(1)
+
+    # # Create a cursor object
+    # cursor = conn.cursor()
+
+    # for parsed_object in parsed_list:
+    #     add(cursor, parsed_object)
+
+    # conn.commit()
+
+    # conn.close()
